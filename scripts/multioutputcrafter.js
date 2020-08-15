1ba74324091036f5834c401fd9e66b7c5c07c8f9
Gdeft <gdeftno@gmail.com>
2020-08-15 15:06:05 +0700

    "Fix" multi output crafter

--- scripts/multioutputcrafter.js
+++ scripts/multioutputcrafter.js
@@ -1,10 +1,34 @@
 const multCrafter = extendContent(GenericCrafter, "multi-output-crafter", {
   init(){
     this.outputItems = [
-      new ItemStack(Items.silicon, 1),
+      new ItemStack(Items.silicon, 2),
       new ItemStack(Items.graphite, 2)
     ];
     this.super$init();
+  },
+
+  load(){
+    this.region = Core.atlas.find(this.name);
+    this.topRegion = Core.atlas.find(this.name + "-top");
+    this.frameRegion = [];
+    for(var i = 1; i < 4; i++){
+      this.frameRegion.push(Core.atlas.find(this.name + "-frame-" + i));
+    }
+  },
+
+  draw(tile){
+    entity = tile.ent();
+
+    Draw.rect(this.region, tile.drawx(), tile.drawy());
+    Draw.rect(this.frameRegion[Mathf.floor(Mathf.absin(entity.totalProgress, 5, this.frameRegion.length))], tile.drawx(), tile.drawy());
+    Draw.rect(this.topRegion, tile.drawx(), tile.drawy());
+  },
+
+  generateIcons(){
+    return [
+      Core.atlas.find(this.name);
+      Core.atlas.find(this.name + "-top");
+    ]
   },
 
   update(tile){
@@ -20,6 +44,7 @@ const multCrafter = extendContent(GenericCrafter, "multi-output-crafter", {
 
     if(entity.progress >= 1){
       entity.cons.trigger();
+
       for(var i = 0; i < this.outputItems.length; i++){
         this.useContent(tile, this.outputItems[i].item);
         for(var j = 0; j < this.outputItems[i].amount; j++){
@@ -45,11 +70,13 @@ const multCrafter = extendContent(GenericCrafter, "multi-output-crafter", {
   }
 });
 
-multCrafter.size = 3;
+multCrafter.size = 5;
 multCrafter.hasItems = true;
 multCrafter.hasLiquids = false;
 multCrafter.hasPower = true;
-multCrafter.consumes.power(2.6);
+multCrafter.craftEffect = Fx.smeltsmoke;
+multCrafter.itemCapacity = 20;
+multCrafter.consumes.power(2.9);
 multCrafter.consumes.items(new ItemStack(Items.sand, 2), new ItemStack(Items.coal, 3));
 multCrafter.requirements = ItemStack.with(Items.copper, 60, Items.lead, 30, Items.silicon, 40);
 multCrafter.category = Category.crafting;
